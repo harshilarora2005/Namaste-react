7ef058a1517800b6971cbc51e96c793accf10c2f
harshilarora2005 <harshil.arora205@gmail.com>
2025-03-15 00:57:55 +0530

    updated package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
             fontFamily: {
@@ -10,5 +12,5 @@ module.exports = {
             },
         },
         },
-        plugins: [],
-};  
+    plugins: [],
+});  
